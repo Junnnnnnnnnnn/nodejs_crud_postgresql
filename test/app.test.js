ed6305c59d2c027ef15a9be402c864ba48a6e832
Junnnnnnnnnnn <wjsguswls71@naver.com>
2021-12-21 17:51:24 +0900

    [UPDATE] 이미지 메타 정보 가져오기

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,19 @@
 var filmDao = require("../routes/film/dao/FlimDao");
 var path = require("path");
 var fs = require("fs");
+var exif = require("exif-parser");
 var ___IMAGE_DIR =  "/Users/jeonhyeonjin/Documents/image";
 
 var upload = () => {
 
-    var file = fs.readFileSync(___IMAGE_DIR + "/image_jetpack.png");
-    var files = fs.readdirSync(___IMAGE_DIR + "/Users/jeonhyeonjin/Documents/image/");
-
+    var file = fs.readFileSync(___IMAGE_DIR + "/image_auth.jpeg");
+    var files = fs.readdirSync(___IMAGE_DIR);
+    // console.log(files);
+    // console.log(exif.create(file).parse());
+    fs.stat(___IMAGE_DIR + "/image_auth.jpeg",(err,state) =>{
+        console.log(state);
+    });
+    
 }
 
 
